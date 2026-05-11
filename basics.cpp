abc74f1d6446f5d9a7600f60ab5aed51f6339e63
snehalXcode <snehalsathawane2511@gmail.com>
2026-05-11 21:27:01 +0530

    Added basics.cpp

---
+++ basics.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"Hello World!"<<endl;
+    cout<<"Welcome to C++ programming."<<endl;
+    cout<<"This is a simple C++ program."<<endl;
+    cout<<3*2;
+
+    return 0;
+}
