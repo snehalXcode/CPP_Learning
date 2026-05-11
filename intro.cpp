5ef8c35e57dbc9e1ca383a4e377e7da0a5e82462
snehalXcode <snehalsathawane2511@gmail.com>
2026-05-11 21:47:42 +0530

    introduction to c++

---
+++ intro.cpp
@@ -0,0 +1,16 @@
+// Print number
+// Two numbers are given, print their product
+// Two numbers are given a and b, print a-b
+// Print 6*3 in output, don’t print 18, just 6*3 should be in the output screen.
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a = 5;
+    int b = 4;
+    cout << a * b << endl; 
+    cout << a - b << endl; 
+
+    cout << "6*3" << endl;
+}
