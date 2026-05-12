0ca245cde4ef6f379160dba1d003aeaa5043c9ff
snehalXcode <snehalsathawane2511@gmail.com>
2026-05-13 00:39:34 +0530

    for loop ques

---
+++ forloop_ques.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;    
+
+// int main(){
+// Print “India will win the World Cup 2023”, 20 times.
+// for (int i = 1 ; i <= 20 ; i++){
+//     cout<<i << "India will win the World Cup 2023"<<endl;
+//    }
+//    return 0 ;
+// }
+// Print all Odd numbers from 1 to n, take n as an input from the user.
+// int main(){
+//     int n ;
+//     cin >> n ;
+//     for (int i = 1 ; i <= n ; i= i + 2){
+//         cout<<i<<endl;
+//     }
+//     return 0;
+// }
+// Print all numbers from 1 to n, which is divisible by 4. Take n as an input from the user.
+// int main (){
+//     int n ;
+//     cin >> n ;
+//     for (int i = 1 ; i <= n ; i++){
+//     if (i%4==0){
+//         cout<<i<<endl;
+//     }
+//   }
+//   return 0;
+// }
