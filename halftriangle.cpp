e31e6258d6dd7fdcc3547953e18c48e398948347
snehalXcode <snehalsathawane2511@gmail.com>
2026-05-14 18:19:23 +0530

    triangles

---
+++ halftriangle.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;    
+
+// int main (){
+//     int n ;
+//     cin >> n ;
+
+//     for (int i = 1 ;i <= n ; i++){
+//         for(int j = 1 ; j <= i ; j++){
+//             cout<<j<<" ";
+//         }
+//         cout<<endl;
+//     }
+//     return 0 ;
+// }
+
+// alphabet pattern
+// int main (){
+//     char ch ;
+//     cin >> ch ;
+
+//     for (char i = 'A' ; i <= ch ; i++){
+//         for (char j = 'A' ; j <= i ; j++){
+//             cout<<j<<" ";
+//         }
+//         cout<<endl;
+//     }
+//     return 0 ;
+// }
+
+// ulta triangle
+// int main (){
+//     int n ;
+//     cin >> n;
+
+//     for (int i = n;i >=1 ; i--){
+//         for(int j = 1; j <=i ; j++){
+//             cout<<j<<" ";
+//         }
+//         cout<<endl;
+//     }
+//     return 0;   
+// }
