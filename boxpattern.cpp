a79dea856ffd7167a2975c25e974f0abb43a976e
snehalXcode <snehalsathawane2511@gmail.com>
2026-05-14 15:52:57 +0530

    square pattern printing

---
+++ boxpattern.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n ;
+    cin >> n;
+
+    for(int i =1 ; i <=n ; i++){
+        // for (int j = 5; j>=1 ; j--){ descinding pattern
+        for(int j = 1 ; j <= n  ;j++){
+            // cout<<n;
+            // cout<<j*j<<" ";
+            // cout<<i*i<<" ";
+            // cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+    return 0 ;
+}
