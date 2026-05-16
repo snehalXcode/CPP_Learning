5d2131ee8ff821ceb757bdbe96350d2ad7c0915d
snehalXcode <snehalsathawane2511@gmail.com>
2026-05-16 19:08:45 +0530

    questions on operators

---
+++ operators.cpp
@@ -0,0 +1,53 @@
+// 1: Temperature Range: Write a program that checks if a given temperature is suitable for swimming. If the temperature is between 70 and 90 (Excluded) degrees Fahrenheit print yes, else NO. 
+#include <iostream>
+using namespace std;
+
+// int main () {
+//     int temp ;
+//     cin >> temp;
+
+//     if (temp > 70 && temp < 90) {
+//         cout << "suitable for swimming" << endl;
+//     }
+//     else {
+//         cout << "not suitable for swimming" << endl;
+//     }
+//     return 0;}
+
+// 2: Even and Positive Number: Write a program that prints “YES” if a given number is both even and positive, otherwise it will print “NO”.
+// int main (){
+//     int n ;
+//     cin >> n ;
+
+//     if (n> 0 && n % 2 == 0 ){
+//         cout << "YES" << endl;
+//     }
+//     else {
+//         cout << "NO" << endl;
+//     }
+//     return 0;
+// }
+
+// Age Check: Implement a program that checks if a person is a teenager. A teenager is someone whose age is between 13 and 19 (inclusive).
+// int main (){
+//     int age ;
+//     cin >> age;
+//     if(age > 12 && age< 20){
+//         cout << "Teenager" << endl;
+//     }
+//     return 0;
+// }
+
+// 4: Take three numbers a,b,c from the user, print yes if a is either greater than b or c. Otherwise print NO.
+// int main (){
+//     int a, b, c;
+//     cin >> a >> b >> c;
+
+//     if (a > b || a > c) {
+//         cout << "YES" << endl;
+//     }
+//     else {
+//         cout << "NO" << endl;
+//     }
+//     return 0;
+// }
